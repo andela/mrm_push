ed207bbc652f66699e27649f1e538d09cf95075d
caveinn <30701246+caveinn@users.noreply.github.com>
2019-02-28 07:56:06 +0300

    feat(notification): make puhs notifications configurable (#2)
    - add table to track subscription infomation
    - add endpoint to register subscribers
    
    [Finishes #164048826]

--- service/push_notification.py
+++ service/push_notification.py
@@ -1,24 +1,39 @@
-from helpers.credentials import Credentials
+import os
+import uuid
+import json
+
+import requests
+from apiclient import errors
 from flask import jsonify, request
 from pyfcm import FCMNotification
-from config import config
-from models.calendar_model import Calendar
-from utilities.utility import (update_entity_fields, stop_channel)
-from apiclient import errors
-import uuid
-import requests
-import os
+from pywebpush import webpush, WebPushException
 
+from config import config
+from helpers.credentials import Credentials
+from models.calendar_model import Calendar
+from models.subscribers_model import Subscriber
+from utilities.utility import (update_entity_fields, stop_channel)
 
 config_name = os.getenv('APP_SETTINGS')
 push_service = FCMNotification(api_key=os.getenv('FCM_API_KEY'))
 api_token = os.getenv('USER_TOKEN')
+vapid_private_key = os.getenv("VAPID_PRIVATE_KEY")
+vapid_email = os.getenv("VAPID_EMAIL")
 
 notification_url = config.get(config_name).NOTIFICATION_URL
 url = config.get(config_name).CONVERGE_MRM_URL
 
 
 class PushNotification():
+    def get_supported_platforms(self):
+        supported_platforms = {
+            "web": self.send_web_notification,
+            "graphql": self.send_graphql_notification,
+            "rest": self.send_rest_notification,
+            "android": self.send_android_notification
+        }
+
+        return supported_platforms
 
     def refresh(self):
         rooms_query = (
@@ -82,7 +97,7 @@ class PushNotification():
 
     def send_notifications(self):
         exact_calendar = Calendar.query.filter_by(
-                    resource_id=request.headers['X-Goog-Resource-Id']).first()
+            resource_id=request.headers['X-Goog-Resource-Id']).first()
         if exact_calendar.firebase_token:
             result = push_service.notify_single_device(
                 registration_id=exact_calendar.firebase_token,
@@ -95,3 +110,74 @@ class PushNotification():
         response = jsonify(data)
 
         return response
+
+    def send_web_notification(self, subscription_info, calendar_id):
+        subscription_info = json.loads(subscription_info)
+        try:
+            webpush(
+                subscription_info=subscription_info,
+                data=str(calendar_id),
+                vapid_private_key=vapid_private_key,
+                vapid_claims={
+                    "sub": "mailto:" + vapid_email
+                }
+            )
+        except WebPushException as exception:
+            print(exception)
+
+    def send_rest_notification(self, subscriber_url, calendar_id):
+        try:
+            requests.post(url=subscriber_url, json=calendar_id)
+        except Exception as e:
+            print(e)
+
+    def send_graphql_notification(self, subscriber_url, calendar_id):
+        calendar_id = str(calendar_id)
+        notification_mutation = "mutation{mrmNotification(calendarId:\"" + calendar_id + "\"){message}}"
+        try:
+            requests.post(url=subscriber_url, json={'query': notification_mutation})
+        except Exception as e:
+            print(e)
+
+    def send_android_notification(self, firebase_token, calendar_id):
+        push_service.notify_single_device(
+            registration_id=firebase_token,
+            message_body=calendar_id)
+
+    def send_notifications_to_subscribers(self):
+        calendar = Calendar.query.filter_by(
+            resource_id=request.headers['X-Goog-Resource-Id']).first()
+        calendar_id = calendar.calendar_id
+        subscribers = Subscriber.query.filter(Subscriber.calendars.any(
+            calendar_id=calendar_id)).all()
+        supported_platforms = self.get_supported_platforms()
+        for subscriber in subscribers:
+            platform = subscriber.platform
+            subscriber_url = subscriber.subscription_info
+            supported_platforms[platform](subscriber_url, calendar_id)
+
+    def subscribe(self, subscriber_info):
+        suported_platforms = self.get_supported_platforms().keys()
+        if not subscriber_info["platform"] in suported_platforms:
+            return "We currently do not support this platform"
+        if subscriber_info["platform"] == "web":
+            subscriber_info["subscription_info"] = json.dumps(subscriber_info["subscription_info"])
+        calendar_ids = subscriber_info.get("calendars")
+        if not calendar_ids:
+            calendars = Calendar.query.all()
+            calendar_ids = [calendar.id for calendar in calendars]
+
+        subscriber_key = str(uuid.uuid4())
+        subscriber_info["subscriber_key"] = subscriber_key
+        subscriber = Subscriber(
+            platform=subscriber_info["platform"],
+            subscription_info=subscriber_info["subscription_info"],
+            subscribed=True,
+            subscriber_key=subscriber_key
+        )
+        for calendar_id in calendar_ids:
+            calendar = Calendar.query.filter_by(id=calendar_id).first()
+            subscriber.calendars.append(calendar)
+        subscriber.save()
+
+        return subscriber_info
