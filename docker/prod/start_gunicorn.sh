c79ce43f2887eac437a24735c7748fcdc65496cd
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-03-05 02:26:18 +0300

    chore(CI/CD) circleci configuration
    - create configuration file for circleci
    - run tests on circleci
    - build docker images
    - push docker images to gcr
    - run terraform to deploy the microservice
    - update current versions of the docker images
    
    [Delivers #164356223]

--- docker/prod/start_gunicorn.sh
+++ docker/prod/start_gunicorn.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
- . .env
- gunicorn manage:app --worker-class gevent -b 0.0.0.0:8000 --reload --log-syslog
+export $(cat .env | xargs)
+cat .env
+gunicorn manage:app --worker-class gevent -b 0.0.0.0:8000 --reload --log-syslog
