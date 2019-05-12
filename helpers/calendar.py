29ee0ffdf6e286a2a33a3fe5aea3dda6031af545
Joshua Ocero <joshuaocero@yahoo.com>
2019-05-12 21:35:32 +0300

    ch(persist-keys): Persist calendar keys in redis db

--- helpers/calendar.py
+++ helpers/calendar.py
@@ -6,6 +6,9 @@ def update_calendar(calendar, calendar_key, room):
             room['firebaseToken'] = ''
         if not calendar:
             key = len(db.keys('*Calendar*')) + 1
-            db.hmset('Calendar:' + str(key), {'calendar_id': room['calendarId'], 'firebase_token': room['firebaseToken']})
-        if 'firebase_token' in calendar.keys() and calendar['firebase_token'] != room['firebaseToken']:
-            db.hmset(calendar_key, {'calendar_id': room['calendarId'], 'firebase_token': room['firebaseToken']})
+            calendar_key = 'Calendar:' + str(key)
+        elif 'firebase_token' not in calendar.keys() or calendar['firebase_token'] == room['firebaseToken']:
+            return None
+
+        db.hmset(calendar_key, {'calendar_id': room['calendarId'], 'firebase_token': room['firebaseToken']})
+        db.persist(calendar_key)
