ebe8d8d0a28d33ae4be6cf17a3c0f90c9cd16d10
Daniel Kamar <35098691+koitoror@users.noreply.github.com>
2019-09-06 15:33:01 +0300

    CON-258-story(bouquets) create bouquet controllers and model: (#23)
    - create controllers and models for the bouquets
    - add tests
    
    [Delivers CON-258]

--- api/v2/__init__.py
+++ api/v2/__init__.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 from flask import Blueprint
 
 from api.v2.controllers.channels.channels_controller import Channels
+from api.v2.controllers.boquets.bouquets_controller import Bouquets
 
 
 api_v2 = Blueprint('mrmpush_2', __name__, url_prefix="/v2")
@@ -12,3 +13,4 @@ mrm_push = Api(api_v2)
 """Add mrm push resources"""
 
 mrm_push.add_resource(Channels, '/channels', strict_slashes=False)
+mrm_push.add_resource(Bouquets, '/bouquets', strict_slashes=False)
